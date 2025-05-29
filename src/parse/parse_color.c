a32e5126e70c8c2da0d5959aea891d55d784c16b
tokochiz <tokohana.1003@gmail.com>
2025-05-29 22:33:25 +0900

    add: RGB値を解析し、マップの床色と天井色を設定する機能を改善

--- src/parse/parse_color.c
+++ src/parse/parse_color.c
@@ -1,5 +1,39 @@
 #include "../../includes/cub3d.h"
 
+static int color_to_int(t_color *color)
+{
+    return ((color->red << 16) | (color->green << 8) | color->blue);
+}
+
+bool parse_rgb_to_color(char *rgb_str, t_color *color)
+{
+ // RGB値を取得
+ char **rgb = ft_split(rgb_str, ',');
+ if (!rgb || !rgb[0] || !rgb[1] || !rgb[2] || rgb[3])
+ {
+     free_split(rgb);
+     free_split(rgb);
+     return (false);
+ }
+
+    // 数値に変換
+    color->red = ft_atoi(rgb[0]);
+     color->green = ft_atoi(rgb[1]);
+ color->blue = ft_atoi(rgb[2]);
+
+    // 値の範囲チェック (0-255)
+    if (color->red < 0 || color->red > 255 || 
+        color->green < 0 || color->green > 255 || 
+        color->blue < 0 || color->blue > 255)
+    {
+        free_split(rgb);
+        return (false);
+    }
+    
+    free_split(rgb);
+    return (true);
+}
+
 bool parse_color(t_map *map, char *line)
 {
     char **split = ft_split(line, ' ');
@@ -8,44 +42,38 @@ bool parse_color(t_map *map, char *line)
         free_split(split);
         return (false);
     }
-    
-    // RGB値を取得
-    char **rgb = ft_split(split[1], ',');
-    if (!rgb || !rgb[0] || !rgb[1] || !rgb[2] || rgb[3])
+    t_color color;
+    if (!parse_rgb_to_color(split[1], &color))
     {
         free_split(split);
-        free_split(rgb);
         return (false);
     }
-    
-    // 数値に変換
-    int r = ft_atoi(rgb[0]);
-    int g = ft_atoi(rgb[1]);
-    int b = ft_atoi(rgb[2]);
-    
-    // 値の範囲チェック (0-255)
-    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
-    {
-        free_split(split);
-        free_split(rgb);
-        return (false);
-    }
-    
-    // 色を保存 (RGB値を1つの整数にエンコード)
-    int color = (r << 16) | (g << 8) | b;
-    
-    if (strncmp(split[0], "F", 2) == 0)
-        map->floor_color = color;
-    else if (strncmp(split[0], "C", 2) == 0)
-        map->ceiling_color = color;
-    else
-    {
-        free_split(split);
-        free_split(rgb);
-        return (false);
-    }
-    
-    free_split(split);
-    free_split(rgb);
-    return (true);
+
+  // 色を保存
+  if (ft_strncmp(split[0], "F", 1) == 0)
+  {
+      if (map->floor_color != -1)  // 重複チェック
+      {
+          free_split(split);
+          return (false);
+      }
+      map->floor_color = color_to_int(&color);
+  }
+  else if (ft_strncmp(split[0], "C", 1) == 0)
+  {
+      if (map->ceiling_color != -1)  // 重複チェック
+      {
+          free_split(split);
+          return (false);
+      }
+      map->ceiling_color = color_to_int(&color);
+  }
+  else
+  {
+      free_split(split);
+      return (false);
+  }
+  
+  free_split(split);
+  return (true);
 }
