fe436a07cf8240fbb33523c5fcaf9313f865889d
tokochiz <tokohana.1003@gmail.com>
2025-06-10 23:37:27 +0900

    refactor: normOKなコードに修正

--- parse/parse_map.c
+++ parse/parse_map.c
@@ -1,49 +1,81 @@
 #include "parse.h"
 
+int calculate_map_width(t_map_data *data, int start_line)
+{
+    int max_width;
+    int i;
+
+    max_width = 0;
+    i = start_line;
+    while(i < data->line_count)
+    {
+        int len = ft_strlen(data->lines[i]);
+        if(len > max_width)
+            max_width = len;
+        i++;
+    }
+    return (max_width);
+}
+
+void cleanup_grid(char **grid, int allocated_rows)
+{
+    int i;
+    
+    i = 0;
+    while (i < allocated_rows)
+    {
+        free(grid[i]);
+        i++;
+    }
+    free(grid);
+}
+
+
+void copy_and_pad_line(char *dest, const char *src, int target_width)
+{
+    int j;
+    int src_len;
+
+    j = 0;
+    src_len = ft_strlen(src);
+
+    while (j < src_len)
+    {
+        dest[j] = src[j];
+        j++;
+    }
+    while (j < target_width)
+    {
+        dest[j] = ' ';
+        j++;
+    }
+    dest[j] = '\0';
+}
+
 bool parse_map_grid(t_map *map, t_map_data *data, int start_line)
 {
-    // マップの行数を計算
     map->height = data->line_count - start_line;
-    if (map->height <= 0)
+    if(map->height <= 0)
         return (false);
-    
-    // マップの幅を計算（最大の行の長さ）
-    map->width = 0;
-    for (int i = start_line; i < data->line_count; i++)
-    {
-        int len = strlen(data->lines[i]);
-        if (len > map->width)
-            map->width = len;
-    }
-    
-    // 2次元配列を確保
+
+    map->width = calculate_map_width(data, start_line);
     map->grid = malloc(sizeof(char *) * map->height);
-    if (!map->grid)
+    if(!map->grid)
         return (false);
-    
-    // マップデータをコピー
-    for (int i = 0; i < map->height; i++)
+
+    int i;
+    i = 0;
+    while (i < map->height)
     {
         map->grid[i] = malloc(sizeof(char) * (map->width + 1));
-        if (!map->grid[i])
+        if(!map->grid[i])
         {
-            // エラー処理: メモリ確保失敗
-            for (int j = 0; j < i; j++)
-                free(map->grid[j]);
-            free(map->grid);
+            cleanup_grid(map->grid, i);
             map->grid = NULL;
             return (false);
         }
-        
-        // 行をコピーし、足りない部分はスペースで埋める
-        int j;
-        int line_len = strlen(data->lines[start_line + i]);
-        for (j = 0; j < line_len; j++)
-            map->grid[i][j] = data->lines[start_line + i][j];
-        while (j < map->width)
-            map->grid[i][j++] = ' ';
-        map->grid[i][j] = '\0';
+        copy_and_pad_line(map->grid[i], data->lines[start_line + i], map->width);
+        i++;
     }
-    
     return (true);
 }
